366c479ee1d256700f4e1b277dbcc87d8df89238
cbruno1978 <chris.bruno@govtact.com>
2025-12-16 18:28:16 -0600

    remove built-in auth

--- boot/src/main/resources/archetype-resources/src/main/java/auth/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ${package}.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-  Optional<UserEntity> findByUsername(String username);
-}
